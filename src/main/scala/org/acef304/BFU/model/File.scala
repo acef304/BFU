33f3be2054ee4d4e9eaced020d6bdd6085bfdb9f
acef304 <zuzler@gmail.com>
2017-01-28 23:31:47 +0300

    Test

--- src/main/scala/org/acef304/BFU/model/File.scala
+++ src/main/scala/org/acef304/BFU/model/File.scala
@@ -1,10 +1,12 @@
 package org.acef304.BFU.model
 
 import java.io.FileInputStream
+import java.math.BigInteger
 
 /**
   * Created by acef304 on 27/11/2016.
   */
+
 class File(name: String, conf: Conf) {
   def initHash: Unit = {
     val t = new FileInputStream(name)
@@ -12,13 +14,25 @@ class File(name: String, conf: Conf) {
     //var count = 0L
     val buff = new Array[Byte](conf.bufSize)
     while (t.read(buff) != -1) {
-      println(getChecksum(buff))
+      println(checkSumToString(getChecksum(buff)))
     }
   }
+
+  def getHash = {
+    val stream = new FileInputStream(name)
+    val buff = new Array[Byte](conf.bufSize)
+    whi
+  }
+
+
+  def checkSumToString(bytes: Array[Byte]) = new BigInteger(1, bytes).toString()
 
   def getChecksum(buff: Array[Byte]) = {
     val m = java.security.MessageDigest.getInstance(conf.digest)
     m.update(buff, 0, buff.length)
-    new java.math.BigInteger(1, m.digest()).toString(16)
+    m.digest()
   }
 }
+
+
+class Hash(filename: String, length: Long, parts: Array[Array[Byte]])
